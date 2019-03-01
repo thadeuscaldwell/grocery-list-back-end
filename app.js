7b6d51da78cc98a783632360de83f1d1447c54d9
Thadeus Caldwell <thadeusc1@gmail.com>
2019-03-01 11:44:56 -0500

    a;

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');//
 const MongoClient = require('mongodb').MongoClient; //connect takes two parameters
-const path = require('path');
 
 
 const app = express();
