ed2856a4b85c7ec20ef8358a34805c601f6e3207
Thadeus Caldwell <thadeusc1@gmail.com>
2019-03-01 11:14:06 -0500

    a;

--- app.js
+++ app.js
@@ -6,26 +6,21 @@ const path = require('path');
 
 const app = express();
 
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use('/static',express.static('public'));
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const groceriesRoutes = require('./routes/groceries');
 
 const PORT = process.env.PORT || 3001
 
 app.use('/groceries',groceriesRoutes);
-
 app.use(function(req,res,next){
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
-let db
+var db
 
 app.get('/' , (req, res) => {
     res.send({
--- routes/groceries.js
+++ routes/groceries.js
@@ -11,12 +11,17 @@ MongoClient.connect('mongodb://Thadeusc:3501Bogan@ds157712.mlab.com:57712/grocer
  
 
 router.get('/', (req, res) => {
-    res.send("Hello Sunshine!")
+    let cursor = db.collection("groceries1").find().toArray(function (err, results) {
+        if (err) {
+            return console.log(err)
+    }
+       res.send(result)
+    })
 })
 
 router.post('/', (req, res) => {
             console.log(req.body)
-    db.collection('groceries').save(req.body, (err, result) => {
+    db.collection('groceries1').save(req.body, (err, result) => {//
         console.log(req.body)       
         if (err) return console.log(err)
             console.log('save to database :)')
